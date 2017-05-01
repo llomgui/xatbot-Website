e4392e51049afc57554857bdb9100d7eb649c6b0
Guillaume Massard <llomgui@live.fr>
2017-05-01 16:59:20 +0000

    Adding debugbar

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
          */
         Ultraware\Roles\RolesServiceProvider::class,
         Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -235,6 +236,7 @@ return [
         'Form'  => Collective\Html\FormFacade::class,
         'Html'  => Collective\Html\HtmlFacade::class,
         'Input' => Illuminate\Support\Facades\Input::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
