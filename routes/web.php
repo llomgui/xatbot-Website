e258f0d8add3761341a56359b1895badce6e7d0e
Guillaume Massard <llomgui@live.fr>
2017-05-01 12:23:11 +0000

    Adding namespaces to routes

--- routes/web.php
+++ routes/web.php
@@ -16,37 +16,37 @@ Route::get('/', function() {
 
 Route::group(['prefix' => 'panel'], function () {
 
-    Route::group(['middleware' => 'auth'], function () {
+    Route::group(['middleware' => 'auth', 'namespace' => 'Page'], function () {
 
-        Route::get('home', 'Page\HomeController@index')->name('panel');
-        Route::get('chat', 'Page\ChatController@index')->name('chat');
+        Route::get('home', 'HomeController@index')->name('panel');
+        Route::get('chat', 'ChatController@index')->name('chat');
 
     });
 
-    Route::group(['prefix' => 'bot', 'middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'bot', 'middleware' => 'auth', 'namespace' => 'Bot'], function () {
 
-        Route::post('create', 'Bot\CreateController@store')->name('bot.create');
-        Route::post('editnickname', 'Bot\EditController@editNickname')->name('bot.editnickname');
-        Route::get('setbotid/{botid}', 'Bot\BotController@setBotID')->name('bot.setbotid');
-        Route::get('edit', 'Bot\EditController@showEditForm')->name('bot.edit');
-        Route::post('edit', 'Bot\EditController@edit')->name('bot.edit');
+        Route::post('create', 'CreateController@store')->name('bot.create');
+        Route::post('editnickname', 'EditController@editNickname')->name('bot.editnickname');
+        Route::get('setbotid/{botid}', 'BotController@setBotID')->name('bot.setbotid');
+        Route::get('edit', 'EditController@showEditForm')->name('bot.edit');
+        Route::post('edit', 'EditController@edit')->name('bot.edit');
 
-        Route::get('staff', 'Bot\StaffController@showStaffForm')->name('bot.staff');
-        Route::post('createstaff', 'Bot\StaffController@createStaff')->name('bot.createstaff');
-        Route::post('editstaff', 'Bot\StaffController@editStaff')->name('bot.editstaff');
-        Route::post('deletestaff', 'Bot\StaffController@deleteStaff')->name('bot.deletestaff');
+        Route::get('staff', 'StaffController@showStaffForm')->name('bot.staff');
+        Route::post('createstaff', 'StaffController@createStaff')->name('bot.createstaff');
+        Route::post('editstaff', 'StaffController@editStaff')->name('bot.editstaff');
+        Route::post('deletestaff', 'StaffController@deleteStaff')->name('bot.deletestaff');
 
-        Route::get('autotemp', 'Bot\AutoTempController@showAutoTempForm')->name('bot.autotemp');
-        Route::post('createautotemp', 'Bot\AutoTempController@createAutoTemp')->name('bot.createautotemp');
-        Route::post('editautotemp', 'Bot\AutoTempController@editAutoTemp')->name('bot.editautotemp');
-        Route::post('deleteautotemp', 'Bot\AutoTempController@deleteAutoTemp')->name('bot.deleteautotemp');
+        Route::get('autotemp', 'AutoTempController@showAutoTempForm')->name('bot.autotemp');
+        Route::post('createautotemp', 'AutoTempController@createAutoTemp')->name('bot.createautotemp');
+        Route::post('editautotemp', 'AutoTempController@editAutoTemp')->name('bot.editautotemp');
+        Route::post('deleteautotemp', 'AutoTempController@deleteAutoTemp')->name('bot.deleteautotemp');
 
-        Route::get('snitch', 'Bot\SnitchController@showSnitchForm')->name('bot.snitch');
-        Route::post('createsnitch', 'Bot\SnitchController@createSnitch')->name('bot.createsnitch');
-        Route::post('editsnitch', 'Bot\SnitchController@editSnitch')->name('bot.editsnitch');
-        Route::post('deletesnitch', 'Bot\SnitchController@deleteSnitch')->name('bot.deletesnitch');
+        Route::get('snitch', 'SnitchController@showSnitchForm')->name('bot.snitch');
+        Route::post('createsnitch', 'SnitchController@createSnitch')->name('bot.createsnitch');
+        Route::post('editsnitch', 'SnitchController@editSnitch')->name('bot.editsnitch');
+        Route::post('deletesnitch', 'SnitchController@deleteSnitch')->name('bot.deletesnitch');
 
-        Route::post('actionbot', 'Bot\BotController@actionBot')->name('bot.actionbot');
+        Route::post('actionbot', 'BotController@actionBot')->name('bot.actionbot');
 
     });
 
@@ -54,24 +54,24 @@ Route::group(['prefix' => 'panel'], function () {
         return 'Support';
     });
 
-    Route::group(['prefix' => 'user'], function () {
+    Route::group(['prefix' => 'user', 'namespace' => 'Auth'], function () {
 
-        Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-        Route::post('login', 'Auth\LoginController@login')->name('login');
+        Route::get('login', 'LoginController@showLoginForm')->name('login');
+        Route::post('login', 'LoginController@login')->name('login');
 
-        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+        Route::post('logout', 'LoginController@logout')->name('logout');
 
-        Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-        Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
 
-        Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-        Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+        Route::post('password/reset', 'ResetPasswordController@reset');
+        Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm');
 
-        Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-        Route::post('register', 'Auth\RegisterController@register')->name('register');
+        Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+        Route::post('register', 'RegisterController@register')->name('register');
 
-        Route::get('profile', 'Auth\ProfileController@showUpdateForm')->name('profile');
-        Route::post('profile', 'Auth\ProfileController@update')->name('profile');
+        Route::get('profile', 'ProfileController@showUpdateForm')->name('profile');
+        Route::post('profile', 'ProfileController@update')->name('profile');
 
     });
 });
