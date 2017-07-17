0eec5eaf497ecac83b6df30e908aacea50aba3b9
Guillaume Massard <llomgui@live.fr>
2017-07-17 14:56:25 +0000

    Adding autobans model and table

---
+++ app/Models/AutoBan.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OceanProject;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property integer $bot_id
+ * @property integer $xatid
+ * @property string $regname
+ * @property integer $hours
+ * @property string $method
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Bot $bot
+ */
+class AutoBan extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = ['bot_id', 'xatid', 'regname', 'hours', 'method', 'created_at', 'updated_at'];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'autobans';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function bot()
+    {
+        return $this->belongsTo('OceanProject\Bot');
+    }
+}
