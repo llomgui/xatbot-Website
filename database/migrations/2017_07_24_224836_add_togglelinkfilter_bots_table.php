e29efb50cf833943cc6cf49e9cbc2e4655ea9f06
Guillaume Massard <llomgui@live.fr>
2017-07-24 23:29:23 +0000

    Adding togglefilterlink in bots table

---
+++ database/migrations/2017_07_24_224836_add_togglelinkfilter_bots_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTogglelinkfilterBotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bots', function (Blueprint $table) {
+            $table->boolean('togglelinkfilter')->default('f');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bots', function (Blueprint $table) {
+            $table->dropColumn('togglelinkfilter');
+        });
+    }
+}
