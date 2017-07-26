47d75fa07d747196e11c1723e51604f122e9fb14
Guillaume Massard <llomgui@live.fr>
2017-07-26 14:37:40 +0000

    Fixing userinfo table

--- database/migrations/2017_06_05_143521_create_userinfo_table.php
+++ database/migrations/2017_06_05_143521_create_userinfo_table.php
@@ -14,6 +14,7 @@ class CreateUserinfoTable extends Migration
     public function up()
     {
         Schema::create('userinfo', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->integer('xatid')->index();
             $table->string('regname')->index();
             $table->integer('chatid');
