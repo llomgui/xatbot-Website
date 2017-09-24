4a603e23455fe53e2cc94bc7506bb451ee5cbec3
xSlOom <illuclem@gmail.com>
2017-09-24 20:10:07 +0200

    PSR2

--- app/Http/Controllers/Staff/BotsController.php
+++ app/Http/Controllers/Staff/BotsController.php
@@ -26,7 +26,8 @@ class BotsController extends Controller
         return view('staff.bots', compact('bots'));
     }
 
-    public function showEditBot(Bot $bot) {
+    public function showEditBot(Bot $bot)
+    {
         $botCheck = Bot::find($bot->id);
 
         if (!is_object($botCheck)) {
@@ -37,12 +38,12 @@ class BotsController extends Controller
         if (session('onBotEdit') == $bot->id) {
             return redirect()
                 ->back()
-                ->withSuccess('You are already editing the Ocean ID ' . $bot->id . '.');      
+                ->withSuccess('You are already editing the Ocean ID ' . $bot->id . '.');
         }
 
         session(['onBotEdit' => $bot->id]);
         return redirect()
             ->back()
-            ->withSuccess('You are now editing OceanID: ' . $bot->id . ' as helper.');        
+            ->withSuccess('You are now editing OceanID: ' . $bot->id . ' as helper.');
     }
 }
