ae314ebd57f9a60d804f31e7b39cdd767552cdfe
llomgui <llomgui@live.fr>
2017-10-01 13:21:45 +0200

    Reviewing profile page

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -66,16 +66,6 @@
 							@endif
 						</div>
 					</div>
-					<div class="form-group">
-						<label for="spotify" class="col-sm-2 control-label">Spotify</label>
-						<div class="col-sm-9">
-							@if (empty($user->spotify))
-							<button type="button" class="btn btn-success waves-effect waves-light" onclick="event.preventDefault(); document.getElementById('spotify-login-form').submit();"><i class="fa fa-spotify fa-lg" aria-hidden="true"></i> Login</button>
-                            @else
-                            <button type="button" class="btn btn-danger waves-effect waves-light" onclick="event.preventDefault(); document.getElementById('spotify-logout-form').submit();"><i class="fa fa-spotify fa-lg" aria-hidden="true"></i> Logout</button>
-                            @endif
-	                    </div>
-					</div>
 					<div class="form-group m-b-0">
 						<div class="col-sm-offset-2 col-sm-9">
 							<button type="submit" class="btn btn-primary waves-effect waves-light">Update</button>
@@ -109,6 +99,25 @@
 							be able to take control of the selected bot(s) including the bot settings/lists/bot behavior etc... <br /><br />
 							<strong>Note</strong>: Remember that at any time, they can remove your access from their bots without reasons.
 						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="card-box">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-sm-12">
+						<h4 class="m-t-0 header-title"><b>Connect your spotify account</b></h4>
+						<p class="text-muted m-b-30 font-13">
+							Show other users what you are currently listening to on Spotify: Just login with your spotify account below and use the !spotify command in the chat.
+						</p>
+						<div class="form-group">
+							@if (empty($user->spotify))
+							<button type="button" class="btn btn-success waves-effect waves-light" onclick="event.preventDefault(); document.getElementById('spotify-login-form').submit();"><i class="fa fa-spotify fa-lg" aria-hidden="true"></i> Login</button>
+                            @else
+                            <button type="button" class="btn btn-danger waves-effect waves-light" onclick="event.preventDefault(); document.getElementById('spotify-logout-form').submit();"><i class="fa fa-spotify fa-lg" aria-hidden="true"></i> Logout</button>
+                            @endif
+						</div>
 					</div>
 				</div>
 			</div>
