3c150e0464e65a65845227374df426d8874bfb2f
xSlOom <illuclem@gmail.com>
2017-10-25 19:52:30 +0200

    Fixed profile and fixed icons on navbar..

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,86 +1,80 @@
 @extends('layouts.panel')
 
 @section('content')
-<div class="row">
+<div class="row" style="margin-top:20px">
 	<div class="col-sm-6">
 		<div class="card-box">
+			<h4 class="m-t-0 header-title"><b>Edit profile</b></h4>
+			<p class="text-muted m-b-30 font-14">You can edit your information via this page</p>
+
 			<div class="row">
-				<h4 class="m-t-0 header-title"><b>Edit Profile</b></h4>
-				<p class="text-muted m-b-30 font-13">You can edit your information via this page</p>
-
-				<form class="form-horizontal" role="form" action="{{ route('profile') }}" method="POST">
-					{!! csrf_field() !!}
-
-					<div class="form-group">
-						<label for="regname" class="col-sm-2 control-label">xat Login</label>
-						<div class="col-sm-9">
-							<input type="text" class="form-control{{ $errors->has('regname') ? ' parsley-error' : '' }}" id="regname" name="regname" placeholder="{{ $user->regname }}">
-							@if ($errors->has('regname'))
-								<ul class="parsley-errors-list filled">
-									<li class="parsley-required">{{ $errors->first('regname') }}</li>
-								</ul>
-							@endif
-						</div>
+				<div class="col-sm-12">
+					<div class="p-12">
+						<form class="form-horizontal" role="form" action="{{ route('profile') }}" method="POST">
+							{!! csrf_field() !!}
+						    <div class="form-group row">
+						        <label class="col-sm-2 col-form-label">xat Login</label>
+						        <div class="col-sm-10">
+						            <input type="text" class="form-control{{ $errors->has('regname') ? ' parsley-error' : '' }}" id="regname" name="regname" placeholder="{{ $user->regname }}">
+						            @if ($errors->has('regname'))
+										<ul class="parsley-errors-list filled">
+											<li class="parsley-required">{{ $errors->first('regname') }}</li>
+										</ul>
+									@endif
+						        </div>
+						    </div>
+						    <div class="form-group row">
+						        <label class="col-sm-2 col-form-label" for="example-email">xatID</label>
+						        <div class="col-sm-10">
+						            <input type="number" class="form-control{{ $errors->has('xatid') ? ' parsley-error' : '' }}" id="xatid" name="xatid" placeholder="{{ $user->xatid }}">
+						            @if ($errors->has('xatid'))
+										<ul class="parsley-errors-list filled">
+											<li class="parsley-required">{{ $errors->first('xatid') }}</li>
+										</ul>
+									@endif
+						        </div>
+						    </div>
+						    <div class="form-group row">
+						        <label class="col-sm-2 col-form-label" for="example-email">Email</label>
+						        <div class="col-sm-10">
+						            <input type="email" class="form-control{{ $errors->has('email') ? ' parsley-error' : '' }}" id="email" name="email" placeholder="{{ $user->email }}">
+						            @if ($errors->has('email'))
+										<ul class="parsley-errors-list filled">
+											<li class="parsley-required">{{ $errors->first('email') }}</li>
+										</ul>
+									@endif
+						        </div>
+						    </div>
+						    <div class="form-group row">
+						        <label class="col-sm-2 col-form-label" for="example-email">Old Password</label>
+						        <div class="col-sm-10">
+						            <input type="password" class="form-control{{ $errors->has('old_password') ? ' parsley-error' : '' }}" id="old_password" name="old_password">
+						            @if ($errors->has('old_password'))
+										<ul class="parsley-errors-list filled">
+											<li class="parsley-required">{{ $errors->first('old_password') }}</li>
+										</ul>
+									@endif
+						        </div>
+						    </div>
+						    <div class="form-group row">
+						        <label class="col-sm-2 col-form-label" for="example-email">New password</label>
+						        <div class="col-sm-10">
+						            <input type="password" class="form-control{{ $errors->has('new_password') ? ' parsley-error' : '' }}" id="new_password" name="new_password">
+						            @if ($errors->has('new_password'))
+										<ul class="parsley-errors-list filled">
+											<li class="parsley-required">{{ $errors->first('new_password') }}</li>
+										</ul>
+									@endif
+						        </div>
+						    </div>
+						    <div class="form-group m-b-0 row">
+                                <div class="offset-3 col-sm-9">
+                                    <button type="submit" class="btn btn-info waves-effect waves-light">Update</button>
+                                </div>
+                            </div>
+						</form>
 					</div>
-					<div class="form-group">
-						<label for="xatid" class="col-sm-2 control-label">xatID</label>
-						<div class="col-sm-9">
-							<input type="number" class="form-control{{ $errors->has('xatid') ? ' parsley-error' : '' }}" id="xatid" name="xatid" placeholder="{{ $user->xatid }}">
-							@if ($errors->has('xatid'))
-								<ul class="parsley-errors-list filled">
-									<li class="parsley-required">{{ $errors->first('xatid') }}</li>
-								</ul>
-							@endif
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="email" class="col-sm-2 control-label">Email</label>
-						<div class="col-sm-9">
-							<input type="email" class="form-control{{ $errors->has('email') ? ' parsley-error' : '' }}" id="email" name="email" placeholder="{{ $user->email }}">
-							@if ($errors->has('email'))
-								<ul class="parsley-errors-list filled">
-									<li class="parsley-required">{{ $errors->first('email') }}</li>
-								</ul>
-							@endif
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="old_password" class="col-sm-2 control-label">Old Password</label>
-						<div class="col-sm-9">
-							<input type="password" class="form-control{{ $errors->has('old_password') ? ' parsley-error' : '' }}" id="old_password" name="old_password">
-							@if ($errors->has('old_password'))
-								<ul class="parsley-errors-list filled">
-									<li class="parsley-required">{{ $errors->first('old_password') }}</li>
-								</ul>
-							@endif
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="new_password" class="col-sm-2 control-label">New Password</label>
-						<div class="col-sm-9">
-							<input type="password" class="form-control{{ $errors->has('new_password') ? ' parsley-error' : '' }}" id="new_password" name="new_password">
-							@if ($errors->has('new_password'))
-								<ul class="parsley-errors-list filled">
-									<li class="parsley-required">{{ $errors->first('new_password') }}</li>
-								</ul>
-							@endif
-						</div>
-					</div>
-					<div class="form-group m-b-0">
-						<div class="col-sm-offset-2 col-sm-9">
-							<button type="submit" class="btn btn-primary waves-effect waves-light">Update</button>
-						</div>
-					</div>
-				</form>
-				@if (empty($user->spotify))
-				<form id="spotify-login-form" action="{{ route('spotify.authorize') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-                @else
-                <form id="spotify-logout-form" action="{{ route('spotify.logout') }}" method="POST" style="display: none;">
-                	{{ csrf_field() }}
-                </form>
-                @endif
+				</div>
 			</div>
 		</div>
 	</div>
@@ -88,7 +82,7 @@
 		<div class="card-box">
 			<div class="row">
 				<div class="form-group">
-					<div class="col-sm-12">
+					<div class="col-12">
 						<h4 class="m-t-0 header-title"><b>Share the bots</b></h4>
 						<p class="text-muted m-b-30 font-13">
 							Give this key to one of your friends and you will be able to have access to their bots.
@@ -106,7 +100,7 @@
 		<div class="card-box">
 			<div class="row">
 				<div class="form-group">
-					<div class="col-sm-12">
+					<div class="col-12">
 						<h4 class="m-t-0 header-title"><b>Connect your spotify account</b></h4>
 						<p class="text-muted m-b-30 font-13">
 							Show other users what you are currently listening to on Spotify: Just login with your spotify account below and use the !spotify command in the chat.
